c48911b5db27bf80b6c6a3fab16a5176e7777c26
minjie0501 <minjie.fan@icloud.com>
2021-12-06 10:05:42 +0100

    database class setup

---
+++ Model/Database.php
@@ -0,0 +1,59 @@
+<?php 
+
+class Database{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = new mysqli("localhost", "root", "", "school");
+    }
+
+    public function getStudents(){
+        $sql = "select * from students";
+        $result = $this->conn->query($sql);
+
+        while ($row = $result->fetch_assoc()) {
+            $students[] = array("id" => $row['id'], "name" => $row['name'],"email" => $row['email'], "class" => $row['class'], "teacher" => $row['teacher']);
+        }
+        return $students;
+    }
+
+    public function getTeachers(){
+        $sql = "select * from teachers";
+        $result = $this->conn->query($sql);
+
+        while ($row = $result->fetch_assoc()) {
+            $teachers[] = array("id" => $row['id'], "name" => $row['name'],"email" => $row['email']);
+        }
+        return $teachers;
+    }
+
+    public function getClasses(){
+        $sql = "select * from classes";
+        $result = $this->conn->query($sql);
+
+        while ($row = $result->fetch_assoc()) {
+            $classes[] = array("id" => $row['id'], "name" => $row['name'],"location" => $row['location'],"teacher" => $row['teacher']);
+        }
+        return $classes;
+    }
+
+
+
+}
+
+?>
+
+
+
+
+
+<!-- $conn = new mysqli(getenv('DATABASE_HOST'), getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD'), getenv('DATABASE_DBNAME'));
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    // echo "Connected successfully";
+    return $conn;
+} -->
--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-require
+<?php 
 
-students
-teachers
-classes
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/View/classes.php">Classes</a>
+    <a href="/View/students.php">Students</a>
+    <a href="/View/teachers.php">Teachers</a>
+</body>
+</html>
