2baeaaff897caac4708e0ed698036ac20b70726d
minjie0501 <minjie.fan@icloud.com>
2021-12-08 16:54:59 +0100

    more stlying changes

--- View/classForm.php
+++ View/classForm.php
@@ -16,51 +16,50 @@ require('../Controller/classForm.php');
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <scr ipt src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></scr>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles/form.css">
 </head>
 
 <body>
     <?php include('includes/header.php'); ?>
 
     <div class="container">
-    <h1>Create a class</h1>
-        <form action="classes.php" method="POST">
-            <input type='hidden' name='id' value=<?php echo $classId; ?>>
-            <input type='hidden' name='teacherId' value=<?php echo $teacherId; ?>>
-            <div class="modal-body">
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" required value=<?php echo $nameValue; ?>>
-                    </div>
-                </div>
-
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="location">Location</label>
-                        <input type="text" class="form-control" id="location" name="location" required value=<?php echo $locationValue; ?>>
-                    </div>
-
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="teachers">Teacher: </label>
-                        <select name="teachers" id="teachers">
-                            <?php teacherOptions($teacherId,$conn); ?>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <div class="form-row" style="text-align:center;display:block;margin:auto;">
-                            <input class="btn btn-primary" type="submit" value=<?php echo $submitValue; ?>>
+        <h1>Create a class</h1>
+            <form action="classes.php" method="POST">
+                <input type='hidden' name='id' value=<?php echo $classId; ?>>
+                <input type='hidden' name='teacherId' value=<?php echo $teacherId; ?>>
+                <div class="modal-body">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required value=<?php echo $nameValue; ?>>
                         </div>
                     </div>
-                </div>
-
-
-
-        </form>
+    
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="location">Location</label>
+                            <input type="text" class="form-control" id="location" name="location" required value=<?php echo $locationValue; ?>>
+                        </div>
+    
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="teachers">Teacher: </label>
+                            <select class="form-select"  name="teachers" id="teachers">
+                                <?php teacherOptions($teacherId, $conn); ?>
+                            </select>
+                        </div>
+                    </div>
+    
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <div class="form-row" style="text-align:center;display:block;margin:auto;">
+                                <input class="btn btn-primary" type="submit" value=<?php echo $submitValue; ?>>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+            </form>
     </div>
 
 </body>
