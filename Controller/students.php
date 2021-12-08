e48bb8add1805b983781bce59f2dafd9d8a70515
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-12-08 16:12:31 +0100

    accpeting new data in students

--- Controller/students.php
+++ Controller/students.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 session_start();
@@ -29,32 +28,6 @@ if (isset($_SESSION['action'])) {
             $db->insertStudent($student);
         }
         unset($_SESSION['action']);
-=======
-<?php 
-require_once ('..\Model\Database.php');
-
-$db= new Database();
-
-class operations extends Database{
-
-public function store(){
-    global $db;
-
-if (isset($_POST['saveData'])) {
-  
-   $name=$db->check($_POST['name']);
-   $email=$db->check($_POST['email']);
-   $className=$db->check($_POST['className']);
-   $teacher=$db->check($_POST['teacher']);
-  
-   if($this->insert($name,$email,$className,$teacher)){
-       
-    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-    <strong>Success!</strong> Your record has been inserted successfully
-  </div>";
-   }
-
->>>>>>> shreejan
     }
 }
 
