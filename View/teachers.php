da3c6d2818f3ad6e9953663e934844ad35978172
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-12-09 14:45:05 +0100

    made a form for search and now trying to make it work

--- View/teachers.php
+++ View/teachers.php
@@ -28,7 +28,10 @@ include "../Controller/teachers.php";
             <h1 class="page-header">
                 Teachers
             </h1>
-
+            <form action="classes.php" method="post">
+                <input type="text" name="search">
+                <input type="submit" value="Search">
+            </form>
             <table class="table table-bordered table-hover">
                 <thead>
                     <tr>
